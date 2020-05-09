990f9155dc43defe571aad2a1fa1294fae854bfc
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-05-09 01:21:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -49,4 +49,10 @@ class Student
     SQL
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+  def self.create(name:, grade:)
+    student = self.new(name,grade)
+    student.save
+    student
+  end
 end
