71ffe8d3c4d341a6fddef054948679e968e14ed0
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-05-09 01:19:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,4 +40,13 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+  def update
+    sql = <<-SQL
+      UPDATE students
+      SET name = ?, grade = ?
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
+  end  
 end
